69fa24a26e2aa7c43108367649c618c743817160
Nicholas Rejack <nrejack@gmail.com>
2021-02-07 12:07:51 -0500

    Breaking classes off into separate files.

---
+++ Slot.java
@@ -0,0 +1,24 @@
+class Slot {
+    // Simulate the slots on a vending machine
+
+    private Soda soda;
+    private int count; // number of cans in slot
+
+    public Slot() {
+        this.soda = null;
+        this.count = 0;
+    }
+
+    public Slot(Soda soda, int count) {
+        this.soda = soda;
+        this.count = count;
+    }
+
+    public int getCount() {
+        return this.count;
+    }
+
+    public int getSoda() {
+        return this.soda;
+    }
+}
--- VendingMachine.java
+++ VendingMachine.java
@@ -10,14 +10,14 @@ class VendingMachine {
 
     public VendingMachine() { // default constructor
         this.slots = Slot[0];
-        this.quarters = this.dimes = this.nickles = this.dollar_coins = this.dollar_bills = 0;
+        this.quarters = this.dimes = this.nickels = this.dollar_coins = this.dollar_bills = 0;
         this.canBeRocked = false;
         this.repairStatus = 0;
     }
 
     public VendingMachine(int slotCount, boolean canBeRocked, double repairStatus) {
         this.slots = Slot[slotCount];
-        this.quarters = this.dimes = this.nickles = this.dollar_coins = this.dollar_bills = 0;
+        this.quarters = this.dimes = this.nickels = this.dollar_coins = this.dollar_bills = 0;
         this.canBeRocked = canBeRocked;
         this.repairStatus = repairStatus;
     }
@@ -28,62 +28,3 @@ class VendingMachine {
     }
 
 }
-
-class Slot {
-    // Simulate the slots on a vending machine
-
-    private Soda soda;
-    private int count; // number of cans in slot
-
-    public Slot() {
-        this.soda = null;
-        this.count = 0;
-    }
-
-    public Slot(Soda soda, int count) {
-        this.soda = soda;
-        this.count = count;
-    }
-
-    public int getCount() {
-        return this.count;
-    }
-
-    public int getSoda() {
-        return this.soda;
-    }
-}
-
-class Soda {
-    // simulate a soda
-    private String name;
-    private String flavor;
-    private double price;
-
-    public Soda() {
-        this.name = "Generic Soda brand Soda";
-        this.flavor = "Tastes like generic soda that fell behind the shelf.";
-        this.price = ".25";
-    }
-
-    public Soda() {
-        this.name = name;
-        this.flavor = flavor;
-        this.price = price;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public String getFlavor() {
-        return this.flavor;
-    }
-
-    public double getPrice() {
-        return this.price;
-    }
-
-
-}
-
